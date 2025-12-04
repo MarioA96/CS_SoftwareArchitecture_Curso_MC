a8ea80d671b40c5bb766c8e8e283d49c30d51696
MarioA96 <marioaquirozs@gmail.com>
2025-12-03 18:18:20 -0600

    Add IRepository and IUseCase abstractions
    Introduced IRepository and IUseCase interfaces to define common async CRUD operations for entities. These abstractions will help standardize data access and use case logic across the application.

---
+++ FullStack/Aplication/Abstractions/IUseCase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Abstractions
+{
+    public interface IUseCase<TEntity>
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(int id);
+    }
+}
