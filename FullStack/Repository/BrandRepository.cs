78b9f22677ae8cf8c1865e0ff4c31f502ea8fdf5
MarioA96 <marioaquirozs@gmail.com>
2025-12-17 22:21:18 -0600

    Add BrandRepository with basic GetByIdAsync method
    Introduces BrandRepository implementing IRepository<BrandEntity> with support for retrieving a brand by ID and mapping between Brand and BrandEntity. Includes basic error handling for missing brands.

---
+++ FullStack/Repository/BrandRepository.cs
@@ -0,0 +1,47 @@
+﻿using Aplication.Abstractions;
+using Data;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class BrandRepository : IRepository<BrandEntity>
+    {
+        private StoreContext _context;
+
+        public BrandRepository(StoreContext storeContext)
+        {
+            _context = storeContext;
+        }
+
+        public async Task<BrandEntity> GetByIdAsync(int id)
+        {
+            var brand = await _context.Brands.FirstOrDefaultAsync(b => b.Id == id);
+            if (brand == null)
+                throw new KeyNotFoundException($"La marca no existe");
+
+            return MapToEntity(brand);
+        }
+
+        #region Mappers
+        private static BrandEntity MapToEntity(Brand model)
+        {
+            return new BrandEntity(model.Id, model.Name);
+        }
+
+        private static Brand MapToModel(BrandEntity entity)
+        {
+            return new Brand
+            {
+                Id = entity.Id ?? 0,
+                Name = entity.Name,
+            };
+        }
+        #endregion
+    }
+}
