103d775eabb961896b36985bbcd6c7838be911a1
MarioA96 <marioaquirozs@gmail.com>
2025-12-17 22:21:02 -0600

    Add BrandEntity class to Domain layer
    Introduces the BrandEntity class with validation for name and id properties. Provides constructors for creating a brand with or without an id, enforcing non-empty names and positive ids.

---
+++ FullStack/Domain/BrandEntity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class BrandEntity
+    {
+        private string _name;
+        public int? Id { get; private set; }
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Nombre no puede ser vacio", nameof(value));
+                }
+                _name = value;
+            }
+        }
+
+        public BrandEntity(string name)
+        {
+            Name = name;
+        }
+
+        public BrandEntity(int id, string name) 
+        {
+            if(id <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(id), "Id debe ser positivo");
+            }
+
+            Id = id;
+            Name = name;
+        }
+    }
+}
