49a43a1eff1f437c71914b5f11dc4e163360ec5c
MarioA96 <marioaquirozs@gmail.com>
2025-12-24 19:28:36 -0600

    Add async CRUD methods to BrandRepository
    Implemented asynchronous methods for retrieving all brands, adding, updating, and deleting brands in the BrandRepository. These methods improve repository functionality and support non-blocking database operations.

--- FullStack/Repository/BrandRepository.cs
+++ FullStack/Repository/BrandRepository.cs
@@ -28,6 +28,37 @@ namespace Repository
             return MapToEntity(brand);
         }
 
+        public async Task<IEnumerable<BrandEntity>> GetAllAsync()
+        {
+            var brands = await _context.Brands.ToListAsync();
+            // Por cada elemento Brand lo convertimos a BrandEntity
+            return brands.Select(MapToEntity); // Equivalente a brands.Select( b=>MapToEntity(b) );
+        }
+
+        public async Task AddAsync(BrandEntity brandEntity)
+        {
+            var brand = MapToModel(brandEntity);
+            await _context.Brands.AddAsync(brand);
+            await _context.SaveChangesAsync(); // Realizamos cambios en la BD
+        }
+
+        public async Task UpdateAsync(BrandEntity brandEntity)
+        {
+            var brand = MapToModel(brandEntity);
+            _context.Entry(brand).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var brand = await _context.Brands.FindAsync(id);
+            if(brand == null)
+                throw new KeyNotFoundException("La marca no existe");
+
+            _context.Brands.Remove(brand);
+            await _context.SaveChangesAsync();
+        }
+
         #region Mappers
         private static BrandEntity MapToEntity(Brand model)
         {
