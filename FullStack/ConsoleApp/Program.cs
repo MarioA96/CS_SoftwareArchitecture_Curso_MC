1207c279d4dd5b9b9e5aa89f4d38d356991d762e
MarioA96 <marioaquirozs@gmail.com>
2025-12-31 19:23:02 -0600

    Add initial Program.cs for ConsoleApp setup
    Introduces the entry point for the ConsoleApp, sets up dependency injection for BrandEntity repository and use case, and resolves the use case from the service provider.

---
+++ FullStack/ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Aplication.Abstractions;
+using Aplication.Brand.UseCases;
+using Domain;
+using Microsoft.Extensions.DependencyInjection;
+using Repository;
+
+var services = new ServiceCollection();
+
+services.AddTransient<IRepository<BrandEntity>, BrandRepository>();
+services.AddTransient<IUseCase<BrandEntity>, BrandUseCase>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+var brandUseCase = serviceProvider.GetRequiredService<IUseCase<BrandEntity>>();
