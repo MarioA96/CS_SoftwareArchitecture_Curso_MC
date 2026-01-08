d3b3846ad6aa19b77b8e2e07d6e7f97dfe80775c
MarioA96 <marioaquirozs@gmail.com>
2026-01-07 23:14:25 -0600

    Add EF Core DbContext and configuration to Program.cs
    Integrated StoreContext with dependency injection using a connection string from appsettings.json. This sets up Entity Framework Core with SQL Server for database operations.

--- FullStack/ConsoleApp/Program.cs
+++ FullStack/ConsoleApp/Program.cs
@@ -2,11 +2,24 @@
 
 using Aplication.Abstractions;
 using Aplication.Brand.UseCases;
+using Data;
 using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Repository;
 
+// conexion
+var configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .Build();
+string connectionString = configuration.GetConnectionString("DefaultConnection");
+
 var services = new ServiceCollection();
+
+services.AddDbContext<StoreContext>(options =>
+            options.UseSqlServer(connectionString)
+          );
 
 services.AddTransient<IRepository<BrandEntity>, BrandRepository>();
 services.AddTransient<IUseCase<BrandEntity>, BrandUseCase>();
