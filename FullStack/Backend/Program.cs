9c21eb80cfb0298390ad2c6575c41539a38d3daa
MarioA96 <marioaquirozs@gmail.com>
2026-01-21 19:52:11 -0600

    Add initial backend Program.cs with brand endpoints
    Creates the Program.cs file for the backend, setting up dependency injection, database context, and endpoints for getting and adding brands. Also includes a test endpoint and OpenAPI configuration.

---
+++ FullStack/Backend/Program.cs
@@ -0,0 +1,50 @@
+using Aplication.Abstractions;
+using Aplication.Brand.UseCases;
+using Data;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<StoreContext>((options) =>
+{
+    options.UseSqlServer(connection);
+});
+
+builder.Services.AddTransient<IRepository<BrandEntity>, BrandRepository>();
+builder.Services.AddTransient<IUseCase<BrandEntity>, BrandUseCase>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("brand", async (IUseCase<BrandEntity> useCase) =>
+{
+    var brands = await useCase.GetAllAsync();
+    return brands;
+}).WithName("getbrand");
+
+app.MapPost("brand", async (IUseCase<BrandEntity> useCase, BrandEntity brand) =>
+{
+    await useCase.AddAsync(brand);
+    return Results.Created();
+}).WithName("addbrand");
+
+app.MapGet("/test", () =>
+{
+    return "online";
+}).WithName("test");
+
+app.Run();
