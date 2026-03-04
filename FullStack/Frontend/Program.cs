c5f737a6311e41bfaec9cd53f66055ec38ebbe4f
MarioA96 <marioaquirozs@gmail.com>
2026-03-03 22:17:29 -0600

    Update Program.cs

--- FullStack/Frontend/Program.cs
+++ FullStack/Frontend/Program.cs
@@ -1,11 +1,26 @@
 using Frontend;
+using Frontend.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+var backendUrl = builder.Configuration["BackendUrl"];
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddHttpClient("Backend", http =>
+{
+    http.BaseAddress = new Uri(backendUrl);
+});
+
+builder.Services.AddScoped(sp =>
+{
+    var http = sp.GetRequiredService<IHttpClientFactory>().CreateClient("Backend");
+    return new ApiClient(http);
+});
 
 await builder.Build().RunAsync();
