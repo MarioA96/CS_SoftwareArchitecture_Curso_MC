f1cfd47eca10868abe86a89ba0a9c069cfd237d7
MarioA96 <marioaquirozs@gmail.com>
2026-03-06 23:11:04 -0600

    Add ProductEntity with validation and constructors
    Introduce ProductEntity in the Domain namespace with properties Id, Name, Cost, Price and Active. Setters include validation and throw ArgumentException (Spanish messages): Id must be > 0; Name required and max 100 chars; Cost must be > 0; Price must be > 0 and not less than Cost. Adds two constructors: one accepting an Id and one for creating a new product without an Id.

---
+++ FullStack/Domain/ProductEntity.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class ProductEntity
+    {
+        private int? _id;
+        private string _name;
+        private decimal _cost;
+        private decimal _price;
+        private bool _active;
+
+        #region properties
+        public int? Id
+        {
+            get => _id;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Id debe ser mayor que cero", nameof(value));
+
+                _id = value;
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Nombre es obligatorio", nameof(value));
+                if (value.Length > 100)
+                    throw new ArgumentException("Nombre no puede tener mas de 100 caracteres", nameof(value));
+
+                _name = value;
+            }
+        }
+
+        public decimal Cost
+        {
+            get => _cost;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("El costo debe ser mayor a 0", nameof(value));
+
+                _cost = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get => _price;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("El precio debe ser mayor a 0", nameof(value));
+                if (value < _cost)
+                    throw new ArgumentException("El costo no puede ser menor que el costo", nameof(value));
+
+                _price = value;
+            }
+        }
+
+        public bool Active
+        {
+            get => _active;
+            set => _active = value;
+        }
+        #endregion
+
+        public ProductEntity(int id, string name, decimal cost, decimal price, bool active)
+        {
+            Id = id;
+            Name = name;
+            Cost = cost;
+            Price = price;
+            Active = active;
+        }
+
+        public ProductEntity(string name, decimal cost, decimal price, bool active)
+        {
+            Name = name;
+            Cost = cost;
+            Price = price;
+            Active = active;
+        }
+    }
+}
