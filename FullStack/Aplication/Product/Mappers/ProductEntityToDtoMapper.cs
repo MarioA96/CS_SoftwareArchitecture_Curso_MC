bcedc45452fe4aa32c2d6c9fe0c40d9f22457011
MarioA96 <marioaquirozs@gmail.com>
2026-03-09 22:52:39 -0600

    Add ProductEntity to ProductDto mapper
    Introduce ProductEntityToDtoMapper implementing IMapper<ProductEntity, ProductDto>. Adds an ArgumentNullException check and maps Id, Name, Cost, Price and Active from Domain.ProductEntity to Aplication.Product.DTOs.ProductDto. New file at FullStack/Aplication/Product/Mappers/ProductEntityToDtoMapper.cs.

---
+++ FullStack/Aplication/Product/Mappers/ProductEntityToDtoMapper.cs
@@ -0,0 +1,28 @@
+﻿using Aplication.Abstractions;
+using Aplication.Product.DTOs;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Product.Mappers
+{
+    public class ProductEntityToDtoMapper : IMapper<ProductEntity, ProductDto>
+    {
+        public ProductDto Map(ProductEntity productEntity)
+        {
+            if (productEntity == null)
+                throw new ArgumentNullException(nameof(productEntity));
+            return new ProductDto
+            {
+                Id = productEntity.Id,
+                Name = productEntity.Name,
+                Cost = productEntity.Cost,
+                Price = productEntity.Price,
+                Active = productEntity.Active,
+            };
+        }
+    }
+}
