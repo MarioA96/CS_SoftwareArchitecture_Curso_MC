f4d1190aa08daaee7f979fc99ff0f5d9d6a3f6d1
MarioA96 <marioaquirozs@gmail.com>
2026-03-10 21:59:43 -0600

    Add generic IReadRepository interface
    Introduce a generic read-only repository abstraction at FullStack/Aplication/Abstractions/IReadRepository.cs. Defines IReadRepository<TEntity> with async methods GetByIdAsync(int id) and GetAllAsync() to standardize async read operations across data stores.

---
+++ FullStack/Aplication/Abstractions/IReadRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Abstractions
+{
+    public interface IReadRepository<TEntity>
+    {
+        public Task<TEntity> GetByIdAsync(int id);
+        public Task<IEnumerable<TEntity>> GetAllAsync();
+    }
+}
