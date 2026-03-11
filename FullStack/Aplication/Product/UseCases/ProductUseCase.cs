dd8084e972386dd9a1f47f92c8978f77d67448db
MarioA96 <marioaquirozs@gmail.com>
2026-03-10 22:00:09 -0600

    Add ProductUseCase read use-case implementation
    Introduce ProductUseCase (new file) implementing IReadUseCase<ProductDto, ProductEntity>. It injects an IReadRepository<ProductEntity> and an IMapper<ProductEntity, ProductDto> to provide async GetByIdAsync (returns null when not found) and GetAllAsync (maps repository entities to DTOs). Placed under Aplication.Product.UseCases to handle read operations for products.

---
+++ FullStack/Aplication/Product/UseCases/ProductUseCase.cs
@@ -0,0 +1,40 @@
+﻿using Aplication.Abstractions;
+using Aplication.Product.DTOs;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Product.UseCases
+{
+    public class ProductUseCase : IReadUseCase<ProductDto, ProductEntity>
+    {
+        private readonly IReadRepository<ProductEntity> _repository;
+        private readonly IMapper<ProductEntity, ProductDto> _mapper;
+
+        public ProductUseCase(IReadRepository<ProductEntity> repository,
+            IMapper<ProductEntity, ProductDto> mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<ProductDto> GetByIdAsync(int id)
+        {
+            var productEntity = await _repository.GetByIdAsync(id);
+            if(productEntity == null)
+            {
+                return null;
+            }
+            return _mapper.Map(productEntity);
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetAllAsync()
+        {
+            var productEntities = await _repository.GetAllAsync();
+            return productEntities.Select(p => _mapper.Map(p));
+        }
+    }
+}
