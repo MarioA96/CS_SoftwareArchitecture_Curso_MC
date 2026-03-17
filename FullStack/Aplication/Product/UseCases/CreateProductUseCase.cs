69659335b87a33886eaa9868e839b2d63f242169
MarioA96 <marioaquirozs@gmail.com>
2026-03-16 23:10:11 -0600

    Add product create and update use cases
    Introduce CreateProductUseCase and UpdateProductUseCase classes under Aplication.Product.UseCases. Each use case is asynchronous, receives a ProductDto, maps it to ProductEntity via an IMapper, and calls the injected repository (ICreateRepository.AddAsync or IUpdateRepository.UpdateAsync). Constructors are set up for dependency injection.

---
+++ FullStack/Aplication/Product/UseCases/CreateProductUseCase.cs
@@ -0,0 +1,32 @@
+﻿using Aplication.Abstractions;
+using Aplication.Product.DTOs;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Product.UseCases
+{
+    public class CreateProductUseCase : ICreateUseCase<ProductDto, ProductEntity>
+    {
+        private readonly ICreateRepository<ProductEntity> _repository;
+        private readonly IMapper<ProductDto, ProductEntity> _mapper;
+
+        public CreateProductUseCase(
+            ICreateRepository<ProductEntity> repository,
+            IMapper<ProductDto, ProductEntity> mapper
+        )
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task AddAsync(ProductDto productDto)
+        {
+            var productEntity = _mapper.Map(productDto);
+            await _repository.AddAsync(productEntity);
+        }
+    }
+}
---
+++ FullStack/Aplication/Product/UseCases/UpdateProductUseCase.cs
@@ -0,0 +1,32 @@
+﻿using Aplication.Abstractions;
+using Aplication.Product.DTOs;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Product.UseCases
+{
+    public class UpdateProductUseCase : IUpdateUseCase<ProductDto, ProductEntity>
+    {
+        private readonly IUpdateRepository<ProductEntity> _repository;
+        private readonly IMapper<ProductDto, ProductEntity> _mapper;
+
+        public UpdateProductUseCase(
+            IUpdateRepository<ProductEntity> repository,
+            IMapper<ProductDto, ProductEntity> mapper
+        )
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task UpdateAsync(ProductDto productDto, int id)
+        {
+            var productEntity = _mapper.Map(productDto);
+            await _repository.UpdateAsync(productEntity, id);
+        }
+    }
+}
