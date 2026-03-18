59cc3539ad0dfddfb4075bb9adbbf23a3280b4d9
MarioA96 <marioaquirozs@gmail.com>
2026-03-17 21:57:37 -0600

    Add DeleteProuctUseCase implementation
    Add a new DeleteProuctUseCase class in Aplication.Product.UseCases that implements IDeleteUseCase and delegates DeleteAsync to an injected IDeleteRepository. Provides constructor injection and asynchronous delete forwarding.

---
+++ FullStack/Aplication/Product/UseCases/DeleteProuctUseCase.cs
@@ -0,0 +1,25 @@
+﻿using Aplication.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Product.UseCases
+{
+    public class DeleteProuctUseCase : IDeleteUseCase
+    {
+        private readonly IDeleteRepository _repository;
+
+        public DeleteProuctUseCase(IDeleteRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _repository.DeleteAsync(id);
+        }
+    }
+}
