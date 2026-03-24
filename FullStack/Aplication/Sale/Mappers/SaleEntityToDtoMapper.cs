2b448ecbc2ef7a22c19b4385779c4376bfd058e4
MarioA96 <marioaquirozs@gmail.com>
2026-03-24 05:02:32 -0600

    Add SaleEntity to SaleDto mapper
    Introduce SaleEntityToDtoMapper (FullStack/Aplication/Sale/Mappers/SaleEntityToDtoMapper.cs) implementing IMapper<SaleEntity, SaleDto>. Maps SaleEntity properties to a SaleDto, converts the Details collection to a list of SaleDetailDto, throws ArgumentNullException for a null input, and guarantees an empty Details list when the source Details is null. This centralizes mapping logic for sale entities to DTOs.

---
+++ FullStack/Aplication/Sale/Mappers/SaleEntityToDtoMapper.cs
@@ -0,0 +1,36 @@
+﻿using Aplication.Abstractions;
+using Aplication.Sale.DTOs;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Sale.Mappers
+{
+    public class SaleEntityToDtoMapper : IMapper<SaleEntity, SaleDto>
+    {
+        public SaleDto Map(SaleEntity saleEntity)
+        {
+            if (saleEntity == null)
+                throw new ArgumentNullException(nameof(saleEntity));
+
+            var saleDto = new SaleDto
+            {
+                Id = saleEntity.Id,
+                Date = saleEntity.Date,
+                Details = saleEntity.Details?.Select(d => new SaleDetailDto { 
+                    Id = d.Id,
+                    ProductId = d.ProductId,
+                    Quantity = d.Quantity,
+                    UnitPrice = d.UnitPrice,
+                    SaleId = d.SaleId,
+                }).ToList() ?? new List<SaleDetailDto>()
+            };
+
+            return saleDto;
+        }
+    }
+}
