6a1d0f09bae3a58df0dd918b7fef96a3a1e11efc
MarioA96 <marioaquirozs@gmail.com>
2026-03-24 05:01:12 -0600

    Add ReadSaleUseCase for Sale
    Introduce ReadSaleUseCase implementing IReadUseCase<SaleDto, SaleEntity>. Uses an IReadRepository and IMapper to provide GetByIdAsync (validates id > 0, throws ArgumentException, returns null if not found) and GetAllAsync (maps entities to DTOs). Adds new file FullStack/Aplication/Sale/UseCases/ReadSaleUseCase.cs.

---
+++ FullStack/Aplication/Sale/UseCases/ReadSaleUseCase.cs
@@ -0,0 +1,44 @@
+﻿using Aplication.Abstractions;
+using Aplication.Sale.DTOs;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Sale.UseCases
+{
+    public class ReadSaleUseCase : IReadUseCase<SaleDto, SaleEntity>
+    {
+        private readonly IReadRepository<SaleEntity, SaleDto> _repository;
+        private readonly IMapper<SaleEntity, SaleDto> _mapper;
+
+        public ReadSaleUseCase(IReadRepository<SaleEntity> repository, IMapper<SaleEntity, SaleDto> mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        public async Task<SaleDto> GetByIdAsync(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException("Id debe ser mayor a 0.", nameof(id));
+            }
+            var saleEntity = await _repository.GetByIdAsync(id);
+            if(saleEntity == null)
+            {
+                return null;
+            }
+            var saleDto = _mapper.Map(saleEntity);
+            return saleDto;
+        }
+
+        public async Task<IEnumerable<SaleDto>> GetAllAsync()
+        {
+            var saleEntities = await _repository.GetAllAsync();
+            var saleDtos = saleEntities.Select(_mapper.Map);
+            return saleDtos;
+        }
+    }
+}
