a907cb977b3deab5b840e01546b22be5c70dc28e
MarioA96 <marioaquirozs@gmail.com>
2026-03-24 04:56:58 -0600

    Register ProductDto validator in DI
    Add FluentValidation support and register ProductDtoValidator in the Frontend Program.cs. This change adds using directives for FluentValidation and Frontend.Validators and registers IValidator<ProductDto> -> ProductDtoValidator in the DI container so the WebAssembly app can resolve and use the validator for ProductDto.

--- FullStack/Frontend/Program.cs
+++ FullStack/Frontend/Program.cs
@@ -1,5 +1,7 @@
+using FluentValidation;
 using Frontend;
 using Frontend.Client;
+using Frontend.Validators;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,6 +11,8 @@ var backendUrl = builder.Configuration["BackendUrl"];
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<IValidator<ProductDto>, ProductDtoValidator>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
