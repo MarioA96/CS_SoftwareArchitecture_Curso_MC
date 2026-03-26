b5dfa30c6d84eaed832e05e34d746bbe5a510173
MarioA96 <marioaquirozs@gmail.com>
2026-03-25 23:08:27 -0700

    Add SaleDto to SaleEntity mapper
    Introduce SaleDtoToEntityMapper implementing IMapper<SaleDto, SaleEntity>. The mapper validates input, constructs a SaleEntity from SaleDto (using Date and Id) and converts any Details into SaleDetailEntity instances (populating product, quantity, unit price and ids) which are added to the SaleEntity.Details collection.

---
+++ FullStack/Aplication/Sale/Mappers/SaleDtoToEntityMapper.cs
@@ -0,0 +1,40 @@
+﻿using Aplication.Abstractions;
+using Aplication.Sale.DTOs;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Sale.Mappers
+{
+    public class SaleDtoToEntityMapper : IMapper<SaleDto, SaleEntity>
+    {
+        public SaleEntity Map(SaleDto saleDto)
+        {
+            if (saleDto == null)
+                throw new ArgumentNullException(nameof(saleDto));
+
+            var saleEntity = new SaleEntity(saleDto.Date, saleDto.Id);
+
+            if (saleDto.Details != null)
+            {
+                foreach (var detailDto in saleDto.Details)
+                {
+                    var detailEntity = new SaleDetailEntity(
+                        saleDto.Id,
+                        detailDto.ProductId,
+                        detailDto.Quantity,
+                        detailDto.UnitPrice,
+                        detailDto.Id
+                        );
+                    saleEntity.Details.Add( detailEntity );
+                }
+                
+            }
+
+            return saleEntity;
+        }
+    }
+}
