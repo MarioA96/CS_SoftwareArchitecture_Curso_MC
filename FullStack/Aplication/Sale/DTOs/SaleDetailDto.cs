4edf77239bd8c1d3ba0ace2941cdf43b8059f3da
MarioA96 <marioaquirozs@gmail.com>
2026-03-26 23:06:45 -0600

    Add SaleDetailDto DTO
    Introduce SaleDetailDto (Aplication.Sale.DTOs) to represent sale detail data. Includes nullable Id and SaleId, ProductId, Quantity, UnitPrice, and a computed TotalPrice property (Quantity * UnitPrice) for transfer between layers.

---
+++ FullStack/Aplication/Sale/DTOs/SaleDetailDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Sale.DTOs
+{
+    public class SaleDetailDto
+    {
+        public int? Id { get; set; }
+        public int? SaleId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice => Quantity * UnitPrice;
+    }
+}
