e0409d87371906022c8f8566142b132df6247c85
MarioA96 <marioaquirozs@gmail.com>
2026-03-26 23:39:08 -0600

    Add CreateSaleUseCase for sales
    Introduce CreateSaleUseCase implementing ICreateUseCase<SaleDto, SaleEntity>. The use case injects an ICreateRepository and IMapper, maps incoming SaleDto to SaleEntity, validates the entity and calls AddAsync on the repository. Throws ArgumentNullException for null DTOs and encapsulates sale creation logic under Aplication.Sale.UseCases.

---
+++ FullStack/Aplication/Sale/UseCases/CreateSaleUseCase.cs
@@ -0,0 +1,36 @@
+﻿using Aplication.Abstractions;
+using Aplication.Sale.DTOs;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Sale.UseCases
+{
+    public  class CreateSaleUseCase : ICreateUseCase<SaleDto, SaleEntity>
+    {
+        private readonly ICreateRepository<SaleEntity> _repository;
+        private readonly IMapper<SaleDto, SaleEntity> _mapper;
+
+        public CreateSaleUseCase(ICreateRepository<SaleEntity> repository, IMapper<SaleDto, SaleEntity> mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task AddAsync(SaleDto saleDto)
+        {
+            if (saleDto == null)
+            {
+                throw new ArgumentNullException(nameof(SaleDto));
+            }
+
+            var saleEntity = _mapper.Map(saleDto);
+            saleEntity.Validate();
+            await _repository.AddAsync(saleEntity);
+        }
+    }
+}
