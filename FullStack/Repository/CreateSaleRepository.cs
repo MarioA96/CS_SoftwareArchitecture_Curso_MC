ccd6eda9d6d291f441a6da73373f89697b458b5b
MarioA96 <marioaquirozs@gmail.com>
2026-03-30 22:53:57 -0600

    Add CreateSaleRepository for creating sales
    Introduce CreateSaleRepository implementing ICreateRepository<SaleEntity> to persist Sale entities via StoreContext. Validates input (throws ArgumentNullException), maps SaleEntity and its Details to the domain Sale and SaleDetail models using MapToModel, adds the Sale to the DbContext and calls SaveChangesAsync.

---
+++ FullStack/Repository/CreateSaleRepository.cs
@@ -0,0 +1,55 @@
+﻿using Aplication.Abstractions;
+using Data;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class CreateSaleRepository : ICreateRepository<SaleEntity>
+    {
+        private readonly StoreContext _context;
+
+        public CreateSaleRepository(StoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(SaleEntity saleEntity)
+        {
+            if(saleEntity == null)
+                throw new ArgumentNullException(nameof(saleEntity));
+
+            var sale = MapToModel(saleEntity);
+            await _context.Sales.AddAsync(sale);
+            await _context.SaveChangesAsync();
+        }
+
+        #region Mappers
+        public static Sale MapToModel(SaleEntity entity)
+        {
+            var sale = new Sale
+            {
+                Date = entity.Date,
+            };
+
+            foreach (var detailEntity in entity.Details)
+            {
+                var saleDetail = new SaleDetail
+                {
+                    ProductId = detailEntity.ProductId,
+                    Quantity = detailEntity.Quantity,
+                    UnitPrice = detailEntity.UnitPrice,
+                };
+
+                sale.SaleDetails.Add(saleDetail);
+            }
+
+            return sale;
+        }
+        #endregion
+    }
+}
