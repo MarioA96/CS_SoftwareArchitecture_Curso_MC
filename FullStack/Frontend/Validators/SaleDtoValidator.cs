0e3a449f3966a0be60359f22741412e3e6b6bb7a
MarioA96 <marioaquirozs@gmail.com>
2026-04-01 22:18:49 -0600

    Add SaleDtoValidator with validation rules
    Introduce SaleDtoValidator (Frontend/Validators/SaleDtoValidator.cs) using FluentValidation to validate SaleDto: ensure Details is not empty, each detail has ProductId > 0 and Quantity > 0, and Date is not in the future (<= now). References Frontend.Client for the SaleDto type.

---
+++ FullStack/Frontend/Validators/SaleDtoValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using Frontend.Client;
+
+namespace Frontend.Validators
+{
+    public class SaleDtoValidator : AbstractValidator<SaleDto>
+    {
+        public SaleDtoValidator()
+        {
+            RuleFor(x => x.Details)
+                .NotEmpty().WithMessage("La venta debe tener productos");
+
+            RuleForEach(x => x.Details).ChildRules(details =>
+            {
+                details.RuleFor(x => x.ProductId)
+                    .GreaterThan(0).WithMessage("El Id del producto debe ser mayor a 0");
+
+                details.RuleFor(x => x.Quantity)
+                    .GreaterThan(0).WithMessage("La cantidad de elementos debe ser mayor a 0");
+            });
+
+            RuleFor(x => x.Date)
+                .LessThanOrEqualTo(DateTime.Now)
+                .WithMessage("La fecha no puede ser del futuro");
+        }
+    }
+}
