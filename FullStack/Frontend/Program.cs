eb68dad6c4a8efd2db676bed0fd69407933df311
MarioA96 <marioaquirozs@gmail.com>
2026-04-02 23:30:05 -0600

    Register SaleDto validator and set es-MX culture
    Add System.Globalization and register IValidator<SaleDto> with SaleDtoValidator in the Blazor WASM Program. Configure CultureInfo.DefaultThreadCurrentCulture and DefaultThreadCurrentUICulture to es-MX so app-wide date/number/culture formatting and localization use Spanish (Mexico). Also keeps existing ProductDto validator and HTTP client registration.

--- FullStack/Frontend/Program.cs
+++ FullStack/Frontend/Program.cs
@@ -4,6 +4,7 @@ using Frontend.Client;
 using Frontend.Validators;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -13,6 +14,11 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<IValidator<ProductDto>, ProductDtoValidator>();
+builder.Services.AddScoped<IValidator<SaleDto>, SaleDtoValidator>();
+
+var culture = new CultureInfo("es-MX");
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
